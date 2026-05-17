90f8ede2c4e3eaca854c1ab45892afaf26d6c1e5
Alfredo Flores <aafv91@gmail.com>
2026-05-17 12:43:58 -0600

    ADD Frontend side panel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,28 @@
 import Header from "./components/Header"
+import SidePanel from "./components/SidePanel"
+
+import "./index.css"
 
 function App() {
   return (
     <div className="app">
+
       <Header />
+
+      <main className="main-content">
+
+        <div className="dashboard-layout">
+
+          <div className="grid-placeholder">
+            Grid
+          </div>
+
+          <SidePanel />
+
+        </div>
+
+      </main>
+
     </div>
   )
 }
